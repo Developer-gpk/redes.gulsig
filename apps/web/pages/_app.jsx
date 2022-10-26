7403f5909f70923e16661fbe882fd438d118cd60
Ruben Trujillo <70417905+RubenElyTrujillo@users.noreply.github.com>
2022-10-26 10:17:45 -0500

    analitycs

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import Script from 'next/script'
 import '../src/styles/global.scss'
 
 export default function MyApp({ Component, pageProps }){
-    return <Component {...pageProps} />
+    return(
+        <>
+            <Script id='analytics-cdn' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=UA-123850556-89`} />
+            <Script id='script-analytics' strategy="lazyOnload">
+                {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                    
+                      gtag('config', 'UA-123850556-89');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    )
 }
